3f4e97ad3891e1633fb851c04e324a903ec9a853
daniel <lesamessipa@gmail.com>
2021-11-04 00:46:50 +0100

    creation du model jobs et stylisation des mission

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,7 +2,7 @@
 <header class="flex justify-between items-center px-5 rounded w-full py-5 bg-gray-100">
   <div> <a href="/" class="hover:text-green-500">logo</a></div>
   <nav>
-    <a href="http://" class="mr-5 hover:text-green-500">nos missons</a>
+    <a href="{{route('jobs.index')}}" class="mr-5 hover:text-green-500">nos missons</a>
     @guest
       <a href="{{route('login')}}" class="mr-5 hover:text-green-500">se connecter</a>
       <a href="{{route('register')}}" class="mr-5 hover:text-green-500">s'enregistrer</a>  
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,5 @@ Route::get('/', function () {
 Route::get('/home', function() {
     return view('home');
 })->middleware('auth')->name('home');
+
+Route::get('jobs', [JobController::class, 'index'])->name('jobs.index');
